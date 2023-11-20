ae910c9c1e2be648b43942b65a088feb13d29047
Keven Nogueira <kevennogueira161@gmail.com>
2023-11-20 18:42:57 -0300

    Criação do AbstractRepository
    Resumo:
    Introduzido o AbstractRepository para encapsular códigos comuns entre MarcaRepository e ModeloRepository. Ambas as classes agora estendem o AbstractRepository.
    
    Descrição detalhada:
    - Criado o arquivo AbstractRepository para abstrair funcionalidades comuns entre MarcaRepository e ModeloRepository.
    - Movidos os códigos compartilhados para o AbstractRepository, visando eliminar duplicação e promover a reutilização de código.
    - As classes MarcaRepository e ModeloRepository agora estendem o AbstractRepository, herdando suas funcionalidades.

--- app/Repositories/MarcaRepository.php
+++ app/Repositories/MarcaRepository.php
@@ -2,40 +2,6 @@
 
 namespace App\Repositories;
 
-use Illuminate\Database\Eloquent\Model;
-
-class MarcaRepository
+class MarcaRepository extends AbstractRepository
 {
-    protected $model;
-
-    public function __construct(Model $model)
-    {
-        $this->model = $model;
-    }
-
-    public function selecaoAttrsRelacionado($attr)
-    {
-        $this->model = $this->model->with($attr);
-    }
-
-    public function filtro($filtro)
-    {
-        $filtros = explode(';', $filtro);
-
-        foreach ($filtros as $key => $condicao) {
-
-            $condicoes = explode(':', $condicao);
-            $this->model = $this->model->where($condicoes[0], $condicoes[1], $condicoes[2]);
-        }
-    }
-
-    public function selecaoAttrs($attrs)
-    {
-        $this->model = $this->model->selectRaw($attrs);
-    }
-
-    public function getResultado()
-    {
-        return $this->model->get();
-    }
 }
